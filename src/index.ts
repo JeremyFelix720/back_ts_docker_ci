3d79a0145350189d7b13772ecd87e67b372ff742
jeremy <jeremyfelix41@gmail.com>
2024-01-03 11:43:34 +0100

    :hammer: add connexionTest function

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,51 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize';  // Voir : https://sequelize.org/docs/v6/getting-started/
 
-const sequelize = new Sequelize('sqlite::memory:');
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+const port = process.env.PORT as string;
+const database = process.env.DATABASE as string;
+const username = process.env.USERNAME as string;
+const password = process.env.PASSWORD as string;
+const server = process.env.SERVER as string;
 
-const items: Item[] = [];
+/*
+console.log("port = " + port)
+console.log("database = " + database)
+*/
+
+const sequelize = new Sequelize(database, username, password, {
+  host: server,
+  dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      },
+      dialectModule: require('pg'),
+  });
+
+async function connexionTest() {
+  try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+  
+connexionTest();
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
   });
 
-app.listen(3000, () => {
-  console.log("Express server started on port 3000");
+app.listen(port, () => {
+  console.log("Express server started on port " + port);
 });
